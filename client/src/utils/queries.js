1fab6732585a5027c36358c5be23b9addd381838
Alexa Chavez <aguinada14@gmail.com>
2024-05-04 14:24:26 -0700

    Added GraphQL queries and mutations for book search, user authentication, and book saving/removal

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,31 +1,85 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
-      _id
-      name
-      skills
+export const QUERY_BOOKS = gql`
+  query searchBooks($searchTerm: String!) {
+    searchBooks(searchTerm: $searchTerm) {
+      title
+      authors
+      description
+      image
+      link
     }
   }
 `;
 
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      skills
+export const QUERY_SAVED_BOOKS = gql`
+  query savedBooks {
+    savedBooks {
+      title
+      authors
+      description
+      image
+      link
     }
   }
 `;
 
-export const QUERY_ME = gql`
-  query me {
-    me {
+export const QUERY_LOGIN = gql`
+  query login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const QUERY_SIGNUP = gql`
+  query signup($username: String!, $email: String!, $password: String!) {
+    signup(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const QUERY_SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
       _id
-      name
-      skills
+      username
+      email
+      savedBooks {
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const QUERY_REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        title
+        authors
+        description
+        image
+        link
+      }
     }
   }
 `;

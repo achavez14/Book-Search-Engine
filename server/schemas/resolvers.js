b8a83f324bd6490c5bb43192c44274c07927a2a4
Alexa Chavez <aguinada14@gmail.com>
2024-05-04 14:04:58 -0700

    Added context as a parameter in searchBooks resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,17 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // Query to search for books based on a search term
-    searchBooks: async (parent, { searchTerm }) => {
-      // Implement logic to search for books using Google Books API or any other source
-      // Return the search results
+    searchBooks: async (parent, { searchTerm }, context) => {
+      if (context.user) {
+        // Implement logic to search for books using Google Books API or any other source
+        const searchBooks = await User.findById(context.user._id);
+        return searchBooks;
+      } else {
+        throw new AuthenticationError('You need to be logged in to search for books.');
+      }
     },
-
-    // Query to get all saved books for the logged-in user
     savedBooks: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate('savedBooks');
@@ -18,9 +20,7 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in to view saved books.');
     },
   },
-
   Mutation: {
-    // Mutation to save a book to the user's account
     saveBook: async (parent, { input }, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
@@ -32,8 +32,6 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in to save books.');
     },
-
-    // Mutation to remove a book from the user's saved books
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
@@ -45,33 +43,23 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in to remove books.');
     },
-
-    // Mutation for user login
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
       if (!user) {
         throw new AuthenticationError('Invalid credentials');
       }
-
       const correctPw = await user.isCorrectPassword(password);
-
       if (!correctPw) {
         throw new AuthenticationError('Invalid credentials');
       }
-
       const token = signToken(user);
       return { token, user };
     },
-
-    // Mutation for user signup
     signup: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
-
-    // Mutation for user logout
     logout: async (parent, args, context) => {
       if (context.user) {
         return { message: 'Successfully logged out' };
